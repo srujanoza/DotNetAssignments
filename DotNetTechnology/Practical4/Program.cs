7c2b2c3c7f783660e1b9f5e511dd066b0d5a1da2
Srujan Oza <47661651+srujanoza@users.noreply.github.com>
2020-01-27 13:12:25 +0530

    Add files via upload

---
+++ DotNetTechnology/Practical4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace Practical4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name, country;
+            do
+            {
+                Console.WriteLine("What is your name?");
+                name = Console.ReadLine();
+            } while (!Regex.IsMatch(name, "^[a-zA-Z]+$"));
+            do
+            {
+                Console.WriteLine("What is your country name?");
+                country = Console.ReadLine();
+            } while (!Regex.IsMatch(country, "^[a-zA-Z]+$"));
+            Console.WriteLine("My name is " + name + " and I am from " + country);
+            Console.ReadLine();
+        }
+
+    }
+}
